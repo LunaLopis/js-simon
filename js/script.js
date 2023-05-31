852886e07b18caf02b4b00b084cf1f0bff2bee58
Luna Lopis <lunalopis59@gmail.com>
2023-05-31 18:07:24 +0200

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,4 +1,45 @@
 
+const randomNumbersContainer = document.getElementById('randomNumbersContainer');
+    const randomNumbers = [];
+    const min = 0;
+    const max = 10;
+  
+    while (randomNumbers.length < 5) {
+      let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      if (!randomNumbers.includes(randomNumber)) {
+        randomNumbers.push(randomNumber);
+      }
+    }
+    console.log(randomNumbers);
+    randomNumbersContainer.textContent = randomNumbers.join(', ');
+
+
+    setTimeout(function(){
+        randomNumbersContainer.textContent = '';
+    },30000);
+
+    setTimeout(function(){
+        const userNumbers = [];
+        for (let i = 0; i < 5; i++) {
+          const userInput = parseInt(prompt('inserisci un numero'));
+          userNumbers.push(userInput);
+        }
+    
+        let matchingNumbers = [];
+        for (let i = 0; i < 5; i++) {
+          if (userNumbers[i] === randomNumbers[i]) {
+            matchingNumbers.push(userNumbers[i]);
+          }
+        }
+    
+        if (matchingNumbers.length === randomNumbers) {
+          alert('tutti corrispondono');
+        } else {
+            alert(`Matching numbers: ${matchingNumbers.join(', ')}\nUser numbers: ${userNumbers.join(', ')}\nRandom numbers: ${randomNumbers.join(', ')}`);
+        }
+    },30100)
+
+
 
 
 
